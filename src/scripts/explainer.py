cb93d2384bbfc756fee13868b720ce1141f257b7
marcinperek <marcinp05@outlook.com>
2025-12-23 03:49:33 +0100

    fix explainer

--- src/scripts/explainer.py
+++ src/scripts/explainer.py
@@ -10,6 +10,6 @@ def get_explanation(patient, model, preprocessor):
         ('preprocessor', preprocessor),
         ('model', model)
     ])
-    X = pd.read_csv('data/X_test_epe_rp.csv')
+    X = pd.read_csv('data/X_epe_rp_full.csv')
     explainer = dx.Explainer(pipeline, X, verbose=False)
     return explainer.predict_parts(patient[X.columns], keep_distributions=True).plot(show=False)
